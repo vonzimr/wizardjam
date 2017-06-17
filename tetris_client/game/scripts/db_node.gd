e7890d09a49571aea30dd4765a8c064b21cea42c
vonzimr <vonzimr@gmail.com>
2017-06-17 03:03:52 -0700

    done for tonight

--- tetris_client/game/scripts/db_node.gd
+++ tetris_client/game/scripts/db_node.gd
@@ -1,42 +1,44 @@
 extends Node2D
 signal poll_database
+var room
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 var thread = Thread.new()
-func _ready():
 	# Called every time the node is added to the scene.
+func _ready():
 	# Initialization here
 	
 	#Connects to the server, keeps connection alive.
-	requests.connectToServer();
 	#Gets array of all blocks.
 	#All blocks
 	#Accessing dict to get piece array
 	#push_blocks_to_grid(null)
-	var room_location = requests.create_room()
-	var blocks = requests.get_blocks("FKEUQ4QXY", 5)
+	room = requests.create_room()
+	
+	var blocks = requests.get_blocks(room['location'], 5, room['token'])
 	print(blocks)
-	print("room loc: " + room_location)
-	#get_node("../Grid").setup_game()
-	#connect("poll_database", self, "push_blocks_to_grid")
+	print(room['location'])
+	get_node("../Grid").setup_game()
+
 
 func push_blocks_to_grid(userdata):
-	var info = requests.get_blocks('AAA', 5);
+	var info = requests.get_blocks(room['location'], 5, room['token']);
+	print(info)
 	for i in range(info.size()):
-		get_node("../Grid").add_block(parse_piece_array(info[i]['piece_array']), info[i]['quote'])
+		get_node("../Grid").add_block(parse_shape(info[i]['shape']), info[i]['quote'])
 	thread = Thread.new()
 	
 func push_blocks_threaded():
 	if not thread.is_active():
 		thread.start(self, "push_blocks_to_grid")
 
-func parse_piece_array(piece_array):
+func parse_shape(shape):
 	var count = 0
 	var vec_array = []
 	for i in range(-2, 2):
 		for x in range(-2, 2):
-			if piece_array[count] == true:
+			if  shape[count] == true:
 				vec_array.push_front(Vector2(x, i))
 			count += 1
 	return vec_array
