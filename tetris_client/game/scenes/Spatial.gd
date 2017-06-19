f782194b030f244836a0b37050c6bef09489d41a
Paul Von Zimmerman <pcvonz@gmail.com>
2017-06-18 22:19:04 -0700

    New ui, blum update

--- tetris_client/game/scenes/Spatial.gd
+++ tetris_client/game/scenes/Spatial.gd
@@ -2,6 +2,7 @@ extends Spatial
 var tetris
 var prev_pos = null
 var anim
+var blum_anim
 var world
 onready var samp = get_node("SpatialSamplePlayer")
 func _ready():
@@ -11,6 +12,7 @@ func _ready():
 	set_process_input(true)
 	tetris = get_node("Viewport/Tetris/Grid")
 	anim = get_node("arcade/AnimationPlayer")
+	blum_anim = get_node("AnimationPlayer")
 	get_node("Area").connect("input_event", self, "_on_area_input_event")
 	get_node("Viewport/Tetris/Grid").connect("move_con", self, "_move_con")
 	get_node("Viewport/Tetris/Grid").connect("play_sample", self, "_play_sample")
@@ -20,12 +22,19 @@ func _ready():
 	get_node("Control/Label").set_text(room_string)
 	
 func toggle_bloom():
-	if(world.is_fx_enabled(world.FX_GLOW)):
+	if blum_anim.get_pos() != 0:
+		blum_anim.play_backwards("blum")
 		get_node("blum").stop()
-		world.set_enable_fx(world.FX_GLOW, false)
 	else:
 		get_node("blum").play()
-		world.set_enable_fx(world.FX_GLOW, true)
+		blum_anim.play("blum")
+	
+#	if(world.is_fx_enabled(world.FX_GLOW)):
+#		get_node("blum").stop()
+#		world.set_enable_fx(world.FX_GLOW, false)
+#	else:
+#		get_node("blum").play()
+#		world.set_enable_fx(world.FX_GLOW, true)
 		
 func _switch_bloom(camera, event, click_pos, click_normal, shape_idx):
 	if event.is_action_pressed("click"):
